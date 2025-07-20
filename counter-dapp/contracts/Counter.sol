4e0321601e3d390a8bb994eb4c258ba84d7ebe0d
João Henrique <129786256+Joao-Falconeres@users.noreply.github.com>
2025-07-20 19:52:35 -0300

    Commit blockchain

---
+++ counter-dapp/contracts/Counter.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Counter {
+    uint256 public count;
+
+    string public ownerName = "Joao Henrique Arantes Falconeres";
+
+    constructor() {
+        count = 0;
+    }
+
+    function increment() public {
+        count += 1;
+    }
+
+    function decrement() public {
+        if (count > 0) {
+            count -= 1;
+        }
+    }
+
+    function getCount() public view returns (uint256) {
+        return count;
+    }
+
+    function reset() public {
+    count = 0;
+    }
+}
